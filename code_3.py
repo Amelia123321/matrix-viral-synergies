0791202ea9aa2916ad5e8c155244bca61da7e0f5
Alersandr <151876621+Amelia123321@users.noreply.github.com>
2024-01-21 22:20:22 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    return data;
+}
+
+function main() {
+
+        console.log("Random Number: " + item);
+    });
+    data.forEach(item => {
+function generateRandomData() {
+    const data = generateRandomData();
+
+main();
+}public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+
+        for (int item : data) {
+}
+    public static void main(String[] args) {
+        }
+        }
+
+    }
