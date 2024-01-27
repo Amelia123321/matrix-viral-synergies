31bae02b2e56aa4fd9c9af7308cc2c0e901811d3
Alersandr <151876621+Amelia123321@users.noreply.github.com>
2024-01-27 04:17:00 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,29 +1,33 @@
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
-    for (let i = 0; i < 10; i++) {
-    const data = [];
-    return data;
 }
+main();
+    data.forEach(item => {
 
+    for (let i = 0; i < 10; i++) {
 function main() {
+    const data = generateRandomData();
+}
+    });
+    const data = [];
+
+    }
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+    return data;
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+    const data = generateRandomData();
+    const data = [];
+    });
+    }
 
         console.log("Random Number: " + item);
-    });
-    data.forEach(item => {
-function generateRandomData() {
-    const data = generateRandomData();
 
-main();
-}public class RandomDataGenerator {
-        for (int i = 0; i < 10; i++) {
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
 
-        for (int item : data) {
+    for (let i = 0; i < 10; i++) {
 }
-    public static void main(String[] args) {
-        }
-        }
-
-    }
+main();
+}
+function main() {
+function generateRandomData() {
